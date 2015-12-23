84601c715dfa2e60537a0adb90aa2f52f7df4fdb
Danielle Closs <dcloss@arbor.net>
2015-12-23 09:33:11 -0500

    updates

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,25 +1,30 @@
-var words = function(input) {   
-    /*
-        split on white space; ignores leading and trailing white space
-    */
-    var wordArray = input.trim().split(/\s+/);
-    /*
-        the object to hold all words and their count
-    */
-    var countObject = {};
+var words = function(input) {
 
-    wordArray.forEach( function(word) { // search every word
-        /*
-            if object does not contain the word already then set its count inside the object
-            or it's an object property
-        */
-        if ( (!(word in countObject)) || (isNaN(countObject[word])) )
-            countObject[word] = 1; // is not in result
-        else
-            countObject[word] ++; // it is already in the result 
-    });
+    //object to return with words and counts
+    this.wordCount = new Object;
 
-    return countObject; // the result of word frequency
+    //local var to store input
+    var phrase = input;
+
+    //split the phrase on spaces, newlines, or tabs to get words in an array
+    var wordArray = phrase.split(/\s+/);
+
+    //loop through the array, and look at each word
+    for(i = 0; i < wordArray.length; i++){
+        var word = wordArray[i];
+
+        this.wordCount[word] = (typeof this.wordCount[word] === "function") ? this.wordCount[word] + 1 : 1;
+
+        // if((typeof this.wordCount[word]) === "function"){
+        //     this.wordCount[word] = 1;
+        // }else if((typeof this.wordCount[word]) !== "undefined"){
+        //     this.wordCount[word] += 1;
+        // }else{
+           //this.wordCount[word] = 1;
+
+        // }
+    }
+    return this.wordCount;
 };
 
 module.exports = words;
